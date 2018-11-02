15e49139d896603ed53d4b2a32b159cc5b118fed
Jatin Tayal <jatintayal4u@gmail.com>
2018-11-01 21:53:39 -0700

    Sherlock and Squares with c++

---
+++ #042/Sherlock_and_squares.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+	long long t,a,b;
+	cin>>t;
+	while(t--)
+	{
+			long long r=(long long)sqrt(b)-(long long)sqrt(a);
+			
+			if(sqrt(a) == (long)sqrt(a))
+				r++;
+			cout<<r<<endl;
+	}
+	return 0;
+}
