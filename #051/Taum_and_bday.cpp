17572044e3f31b8962cdb6c0a1a68f64315f6b1f
Jatin Tayal <jatintayal4u@gmail.com>
2018-11-01 21:57:56 -0700

    Taum and bday with c++

---
+++ #051/Taum_and_bday.cpp
@@ -0,0 +1,49 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int t;
+    cin >> t;
+    for(int a0 = 0; a0 < t; a0++){
+        long b;
+        long w;
+        cin >> b >> w;
+        long x;
+        long y;
+        long z;
+        cin >> x >> y >> z;
+        
+        if(x >= (y + z)){
+            cout << b * (y + z) + w * y << endl;
+        }
+        else if(y >= (x + z)){
+            cout << b * x + w * (x + z) << endl; 
+        }
+        else
+            cout << b * x + w * y << endl;
+    }
+    return 0;
+}
